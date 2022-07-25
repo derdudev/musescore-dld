178f72235511eefb0fae34808ccc90bac517d8eb
Matteo <yurugen.vurusto@gmail.com>
2022-07-25 02:16:39 +0200

    New scanning method (based on ms-jmuse-api)

--- src/content-scripts/musescore-dld-main.js
+++ src/content-scripts/musescore-dld-main.js
@@ -1,18 +1,25 @@
-const SCROLLER_ID = "jmuse-scroller-component";
-const TITLE_CL = "C4LKv DIiWA Pz1rt";
-const ARTIST_CL = "ASx44 AJXCt Bz0hi g1QZl";
-
 (function () {
+    const SCROLLER_ID = "jmuse-scroller-component";
+    const TITLE_CL = "C4LKv DIiWA Pz1rt";
+    const ARTIST_CL = "ASx44 AJXCt Bz0hi g1QZl";
+    const DLD_BTN_ID = "9eb7dc50a34f5b5c1cff5f66d2e17b0b";
+    const DLD_BTN_CL = "TtlUw TtlUw R1reb r6afg HFvdW Dhs0s nOTLW utani s_klh u_VDg";
+    const DLD_SEC_CL = "lPCDt oJw2k UvTPO XyPHW g1QZl eDGkx";
+
     let scroller = document.getElementById(SCROLLER_ID);
+
+    const dldBtn = document.getElementById(DLD_BTN_ID);
+    const dldSection = document.getElementsByClassName(DLD_SEC_CL)[0];
 
     if(scroller){
         let title = document.getElementsByClassName(TITLE_CL)[0];
         let artist = document.getElementsByClassName(ARTIST_CL)[0].children[1].children[0];
+        let pageNum = document.getElementsByClassName("BmIOX").length;
 
         let info = {
             title: title.innerText,
             artist: artist.innerText,
-            pages: document.getElementsByClassName("BmIOX").length
+            pages: pageNum
         }
 
         browser.runtime.sendMessage({
@@ -55,50 +62,51 @@ const ARTIST_CL = "ASx44 AJXCt Bz0hi g1QZl";
             });
         }
 
-        const crawlSheets = () => {
+        const getSheetURL = async (index) => {
+            return fetch(`https://musescore.com/api/jmuse?id=769686&index=${index}&type=img&v2=1`, {
+                headers: {
+                    "Authorization": "8c022bdef45341074ce876ae57a48f64b86cdcf5"
+                }
+            }).then(r => r.json())
+            .then(r => {
+                return r.info.url
+            });
+        }
+
+        // const getSheetDataURL = async (url, fileType) => {
+        //     browser.runtime.sendMessage({command:"GetDataURL", data:JSON.stringify({url:url, ft:fileType})});
+
+        //     return new Promise(resolve => {
+        //         browser.runtime.onMessage.addListener(msg => {
+        //             if(msg.command = "DataUrl"){
+        //                 resolve(msg.dataURL);
+        //             }
+        //         });
+        //     });
+        // }
+
+        const crawlSheets = async () => {
             scroller.scrollTo(0, 0);
-            pages[0].src = scroller.children[0].children[0].src;
-            pages[0].scraped = true;
-            if(document.getElementsByClassName("BmIOX").length != 1){
-                let id = setInterval(async ()=>{
-                    if(lastPage.children.length == 0){
-                        scroller.scrollBy({
-                            top: 100,
-                            behavior: "smooth"
-                        });
-                        let currentSheet;
-                        for(let i=1; i<lastPageIndex; i++){
-                            if(scroller.children[i].children.length != 0 && !pages[i].scraped) {
-                                currentSheet = scroller.children[i].children[0];
-                                if(currentSheet.src != "") {
-                                    pages[i].src = currentSheet.src;
-                                    pages[i].scraped = true;
-                                }
-                                else {
-                                    currentSheet.onload = () => {
-                                        pages[i].src = currentSheet.src;
-                                        pages[i].scraped = true;
-                                    }
-                                }
-                            }
-                        }
-                    } else {
-                        pages[lastPageIndex].src = scroller.children[lastPageIndex].children[0].src;
-                        if(pages[lastPageIndex].src == "") {
-                            scroller.children[lastPageIndex].children[0].onload = () => {
-                                pages[lastPageIndex].src = scroller.children[lastPageIndex].children[0].src;
-                                pages[lastPageIndex].scraped = true;
-                                sendToEx();
-                                clearInterval(id);
-                            }
-                        } else {
-                            pages[lastPageIndex].scraped = true;
-                            clearInterval(id);
-                        }
-                    }
-                }, 10);
-            } else if (document.getElementsByClassName("BmIOX").length == 1){
-                sendToEx();
+            // first sheet over src on load
+
+            // all following sheets over API
+
+            let sheets = [];
+            for(let i=0; i<pageNum; i++){
+                sheets.push(await getSheetURL(i));
+            }
+            for(let i=0; i<sheets.length; i++){
+                let dataURL = await getSheetDataURL(sheets[i], "png");
+                console.log(dataURL);
+            }
+
+            if(dldBtn){
+                let hBtn = document.createElement("button");
+                hBtn.className = DLD_BTN_CL;
+                hBtn.innerHTML = dldBtn.innerHTML;
+                hBtn.onclick = () => console.log(sheets);
+
+                dldSection.replaceChild(hBtn, dldBtn);
             }
         }
         crawlSheets();
