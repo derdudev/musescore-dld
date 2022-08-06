0f9e63d92c40d0cc768972c6665add713c3d026c
Matteo <yurugen.vurusto@gmail.com>
2022-08-06 14:04:32 +0200

    unresolved: issue of event handler firing multiple times

--- src/content-scripts/musescore-dld-main.js
+++ src/content-scripts/musescore-dld-main.js
@@ -1,3 +1,4 @@
+console.log("hello");
 (async () => {
     try {
         const SCROLLER_ID = "jmuse-scroller-component";
@@ -6,6 +7,11 @@
         const DLD_BTN_ID = "9eb7dc50a34f5b5c1cff5f66d2e17b0b";
         const DLD_BTN_CL = "TtlUw TtlUw R1reb r6afg HFvdW Dhs0s nOTLW utani s_klh u_VDg";
         const DLD_SEC_CL = "lPCDt oJw2k UvTPO XyPHW g1QZl eDGkx";
+
+        const S3_SCORE_ID_MATCH = /(?<=https:\/\/s3\.ultimate-guitar\.com\/musescore\.scoredata\/g\/).*(?=\/)/;
+        const CONTENT_CREDENTIAL_MATCH = /(?<=https:\/\/s3\.ultimate-guitar\.com\/musescore\.scoredata\/g\/.*\/score_0\.svg\?response-content-disposition=attachment%3B%20filename%3D%22score_0\.svg%22&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=).*(?=&X-Amz-Date)/;
+        const DATE_MATCH = /(?<=https:\/\/s3\.ultimate-guitar\.com\/musescore\.scoredata\/g\/.*\/score_0\.svg\?response-content-disposition=attachment%3B%20filename%3D%22score_0\.svg%22&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=.*&X-Amz-Date=).*(?=&X-Amz-SignedHeaders)/;
+        const SIGNATURE_MATCH = /(?<=https:\/\/s3\.ultimate-guitar\.com\/musescore\.scoredata\/g\/.*\/score_0\.svg\?response-content-disposition=attachment%3B%20filename%3D%22score_0\.svg%22&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=.*&X-Amz-Date=.*&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Signature=).*/;
     
         let sheets = [];
         let sheetsDataURLs = [];
@@ -17,12 +23,12 @@
         let dldSection // = document.getElementsByClassName("js-page react-container")[0].children[0].children[1].children[1].children[0].children[3].children[0];
         let dldBtn // = dldSection.children[0];
 
-        const init = async () => {
-            let smPopup = document.createElement("div");
-            smPopup.innerText = "Initiating MsDLD v6";
-            smPopup.className = "sm-popup";
-            document.body.appendChild(smPopup);
+        let smPopup = document.createElement("div");
+        smPopup.innerText = "Initiating MsDLD v6";
+        smPopup.className = "sm-popup";
+        document.body.appendChild(smPopup);
 
+        const init = async () => {
             return new Promise(resolve => {
                 let interval = setInterval(()=>{
                     console.log("loading")
@@ -39,7 +45,6 @@
                         pageNum = document.getElementsByClassName("BmIOX").length;
                         dldSection = document.getElementsByClassName("js-page react-container")[0].children[0].children[1].children[1].children[0].children[3].children[0];
                         dldBtn = dldSection.children[0];
-                        setTimeout(()=>document.body.removeChild(smPopup), 500)
                         clearInterval(interval);
                         resolve();
                     } 
@@ -130,22 +135,56 @@
         function byteCount(s) {
             return encodeURI(s).split(/%..|./).length - 1;
         }
+
+        function addToLStorage (sheetInfo, sheetData, pdfData) {
+            let storeData = {};
+            storeData["msdld-"+scoreID] = JSON.stringify({
+                info: sheetInfo, 
+                sheets: sheetData,
+                pdf: pdfData,
+            });
+
+            browser.storage.local.set(storeData); 
+        }
     
         const pDldBtn = document.getElementById("dld-btn"); // extension popup dld button
         pDldBtn.className = "unactive";
         pDldBtn.onclick = async () => {
             logToUser("PDF Download initiated...");
             logToUser(`Downloading ${pageNum} pages...`);
-    
-            let currentSheetURL;
+            
             let completeString;
-            for(let i=0; i<pageNum; i++){
-                logToUser(`Downloading page ${i+1} of ${pageNum}...`);
-                currentSheetURL = await getSheetDataURL(sheets[i], fileType)
-                sheetsDataURLs[i] = currentSheetURL;
-                completeString += currentSheetURL;
-                logToUser(`Download of page ${i+1} of ${pageNum} complete.`, "success");
-            }
+            await browser.storage.local.get("msdld-"+scoreID)
+                .then(async lsPromiseData => {
+                    let lsData = (Object.entries(lsPromiseData) == 0) ? null : lsPromiseData["msdld-"+scoreID];
+                    console.log(lsData);
+                    if(lsData){
+                        let lsDataParsed = JSON.parse(lsData);
+                        sheetsDataURLs = lsDataParsed.sheets;
+                        for(let i=0; i<pageNum; i++){
+                            completeString += sheetsDataURLs[i];
+                        }
+                        logToUser("Received from storage", "success");
+                    } else {
+                        let currentSheetURL;
+                        for(let i=0; i<pageNum; i++){
+                            logToUser(`Downloading page ${i+1} of ${pageNum}...`);
+                            currentSheetURL = await getSheetDataURL(sheets[i], fileType)
+                            sheetsDataURLs[i] = currentSheetURL;
+                            completeString += currentSheetURL;
+                            logToUser(`Download of page ${i+1} of ${pageNum} complete.`, "success");
+                        }
+
+                        logToUser("Saved for next download.", "success")
+                        addToLStorage({
+                            artist: artist,
+                            title: title
+                        },
+                        sheetsDataURLs);
+                    }
+                    return;
+                });
+
             let estimatedDldTime = Math.floor(byteCount(completeString)/99333);
             logToUser("Estimated download time: " + estimatedDldTime + " seconds");
             browser.runtime.sendMessage({
@@ -160,10 +199,11 @@
                     urls: sheetsDataURLs
                 })
             });
+            
             let counter = 2;
             let interval = setInterval(() => {
                 logToUser(`Bear with us, the download is progressing... ${"(" + counter + "s passed)"}`);
-                if(counter < estimatedDldTime+10) {
+                if(counter < estimatedDldTime+20) {
                     if(counter == 16) logToUser(`This is taking a little longer but nothing to worry about.`, "warning");
                     else if (counter == 26) logToUser(`Still going - must be a huge file. And YOU still got the brains to play it tho, amazing!`, "warning");
                     else if (counter == 36) logToUser(`Mmh, this really is taking its time, isnt it?`, "warning");
@@ -172,7 +212,8 @@
                     else if (counter == 66) logToUser(`This is the point at which you should head to Twitter and cancel this downloader. It's probably racist too!`, "warning");
                     counter += 2;
                 } else {
-                    logToUser(`Download TIMEOUT. Estimated download time: ${estimatedDldTime}s | Process interupted at ${counter}s`)
+                    logToUser(`Download TIMEOUT. Estimated download time: ${estimatedDldTime}s | Process interupted at ${counter}s`, "error")
+                    clearInterval(interval);
                 }
             }, 2000);
             browser.runtime.onMessage.addListener(msg => {
@@ -192,78 +233,80 @@
                     clearInterval(interval);
                     throw new Error("Backend Error: " + msg.error);
                 }
-            })    
+                return true;
+            });  
         }
     
-        if(scroller){
-            const pages = [];
-            for(let i=0; i<scroller.children.length-2; i++){
-                pages.push({
-                    element: scroller.children[i],
-                    scraped: false,
-                    src: "",
-                });
-            }
-    
-            let lastPageIndex = scroller.children.length - 3;
-            let lastPage = scroller.children[lastPageIndex];
-    
-            if(lastPage.className == ""){
-                lastPage.parentNode.removeChild(lastPage);
-                lastPageIndex = lastPageIndex-1;
-                lastPage = scroller.children[lastPageIndex];
-            }
-    
-            const getSheetURL = async (index) => {
-                return fetch(`https://musescore.com/api/jmuse?id=${scoreID}&index=${index}&type=img&v2=1`, {
-                    headers: {
-                        "Authorization": "8c022bdef45341074ce876ae57a48f64b86cdcf5"
-                    }
-                }).then(r => r.json())
-                .then(r => r.info.url);
-            }
-    
-            const crawlSheets = async () => {
-                let currentSheet = "";
-                let img, imgContainer, psElem;
-                let displaySheets = (pageContent.children.length == 0);
-                for(let i=0; i<pageNum; i++){
-                    currentSheet = await getSheetURL(i);
-                    sheets.push(currentSheet);
-                    if(displaySheets) {
-                        img = document.createElement("img");
-                        img.src = currentSheet;
-                        img.className = "sheet";
-                        imgContainer = document.createElement("div");
-                        imgContainer.className = "sheet-container";
-                        imgContainer.setAttribute("page-num", i+1);
-                        imgContainer.appendChild(img);
-                        pageContent.appendChild(imgContainer);
-                    }
-    
-                    if(i==0) {
-                        fileType = currentSheet.match(/(?<=https:\/\/s3\.ultimate-guitar\.com\/musescore\.scoredata\/g\/.+\/score_0\.).{3}/)[0];
-                        console.log(fileType);
-                    }
-                }
-    
-                console.log(dldBtn);
-                if(dldBtn){
-                    let hBtn = document.createElement("button");
-                    hBtn.className = DLD_BTN_CL;
-                    hBtn.style.backgroundColor = "#26a875";
-                    hBtn.innerHTML = dldBtn.innerHTML;
-                    hBtn.onclick = () => {
-                        popupContainer.style.display = "flex";
-                    };
-    
-                    dldSection.replaceChild(hBtn, dldBtn);
-                }
-    
-                if(sheets) pDldBtn.classList = "";
-            }
-            crawlSheets();
+        const pages = [];
+        for(let i=0; i<scroller.children.length-2; i++){
+            pages.push({
+                element: scroller.children[i],
+                scraped: false,
+                src: "",
+            });
         }
+
+        let lastPageIndex = scroller.children.length - 3;
+        let lastPage = scroller.children[lastPageIndex];
+
+        if(lastPage.className == ""){
+            lastPage.parentNode.removeChild(lastPage);
+            lastPageIndex = lastPageIndex-1;
+            lastPage = scroller.children[lastPageIndex];
+        }
+
+        const getSheetURL = async (index) => {
+            return fetch(`https://musescore.com/api/jmuse?id=${scoreID}&index=${index}&type=img&v2=1`, {
+                headers: {
+                    "Authorization": "8c022bdef45341074ce876ae57a48f64b86cdcf5"
+                }
+            }).then(r => r.json())
+            .then(r => {
+                console.log(r.info.url);
+                return r.info.url;
+            });
+        }
+
+        const crawlSheets = async () => {
+            let currentSheet = "";
+            let img, imgContainer, psElem;
+            let displaySheets = (pageContent.children.length == 0);
+            for(let i=0; i<pageNum; i++){
+                currentSheet = await getSheetURL(i);
+                sheets.push(currentSheet);
+                if(displaySheets) {
+                    img = document.createElement("img");
+                    img.src = currentSheet;
+                    img.className = "sheet";
+                    imgContainer = document.createElement("div");
+                    imgContainer.className = "sheet-container";
+                    imgContainer.setAttribute("page-num", i+1);
+                    imgContainer.appendChild(img);
+                    pageContent.appendChild(imgContainer);
+                }
+
+                if(i==0) {
+                    fileType = currentSheet.match(/(?<=https:\/\/s3\.ultimate-guitar\.com\/musescore\.scoredata\/g\/.+\/score_0\.).{3}/)[0];
+                    console.log(fileType);
+                }
+            }
+
+            if(dldBtn){
+                let hBtn = document.createElement("button");
+                hBtn.className = DLD_BTN_CL;
+                hBtn.style.backgroundColor = "#26a875";
+                hBtn.innerHTML = dldBtn.innerHTML;
+                hBtn.onclick = () => {
+                    popupContainer.style.display = "flex";
+                };
+
+                dldSection.replaceChild(hBtn, dldBtn);
+                document.body.removeChild(smPopup); // remove loading msg
+            }
+
+            if(sheets) pDldBtn.classList = "";
+        }
+        crawlSheets();
     } catch (e) {
         console.error(e);
     }
--- src/msdld-bg.js
+++ src/msdld-bg.js
@@ -11,73 +11,73 @@ try {
     // https://stackoverflow.com/questions/5913338/embedding-svg-in-pdf-exporting-svg-to-pdf-using-js
     let docKit = new PDFDocument({compress: false, size:"A4"});
     
-    browser.runtime.onMessage.addListener(msg => {
+    browser.runtime.onMessage.addListener(async msg => {
         if(msg.command == "download"){
-            (async ()=>{
-                const tabs = await browser.tabs.query({active: true, currentWindow: true}).then(tabs => tabs)
-                try {
-                    browser.tabs.sendMessage(tabs[0].id, {
-                        command: "received",
-                    })
-                    
-                    
-                    let data = JSON.parse(msg.data);
-        
-                    let tempSVG;
-                    for(let i=0; i<data.urls.length; i++){
-                        if(data.fileType == "png") {
-                            await docKit.image(data.urls[i], 0, 0, {width: 595});
-                        }
-                        else {
-                            tempSVG = document.createElement("div");
-                            tempSVG.innerHTML = atob(data.urls[i].match(/(?<=data:image\/svg\+xml;base64,).*/));
-                            
-                            console.log(tempSVG.firstChild);
-                            let width = tempSVG.firstChild.getAttribute("width");
-                            let height = tempSVG.firstChild.getAttribute("height");
-        
-                            tempSVG.firstChild.setAttribute("width", "");
-                            tempSVG.firstChild.setAttribute("height", "");
-                            tempSVG.firstChild.setAttribute("viewBox", "0 0 " + width + " " + height);
-        
-                            console.log(tempSVG.firstChild);
-        
-                            browser.tabs.sendMessage(tabs[0].id, {
-                                command: "addingPage",
-                                pageNum: i+1,
-                            })
-                            await SVGtoPDF(docKit, tempSVG.firstChild, 0, 0);
-                            browser.tabs.sendMessage(tabs[0].id, {
-                                command: "addedPage",
-                                pageNum: i+1,
-                            })
-                        }
-                        if(i < data.urls.length-1) {
-                            docKit.addPage();
-                        }
+            const tabs = await browser.tabs.query({active: true, currentWindow: true}).then(tabs => tabs)
+            try {
+                browser.tabs.sendMessage(tabs[0].id, {
+                    command: "received",
+                })
+                
+                
+                let data = JSON.parse(msg.data);
+    
+                let tempSVG;
+                for(let i=0; i<data.urls.length; i++){
+                    if(data.fileType == "png") {
+                        await docKit.image(data.urls[i], 0, 0, {width: 595});
                     }
-                    
-                    let stream = docKit.pipe(blobStream());
-                    stream.on('finish', async () => {
-                        let blob = stream.toBlob('application/pdf');
-                        downloadFile(data.info, data.type, URL.createObjectURL(blob));
-                        browser.tabs.sendMessage(tabs[0].id, {
-                            command: "dldSucc",
-                            data: ""
-                        })
+                    else {
+                        tempSVG = document.createElement("div");
+                        tempSVG.innerHTML = atob(data.urls[i].match(/(?<=data:image\/svg\+xml;base64,).*/));
                         
-                    });
-                    docKit.end();
-                    docKit = new PDFDocument({compress: false, size:"A4"}); // new instance for new stream!
-                } catch (e) {
-                    browser.tabs.sendMessage(tabs[0].id, {
-                        command: "backendError",
-                        error: e.message
-                    }) 
+                        console.log(tempSVG.firstChild);
+                        let width = tempSVG.firstChild.getAttribute("width");
+                        let height = tempSVG.firstChild.getAttribute("height");
+    
+                        tempSVG.firstChild.setAttribute("width", "");
+                        tempSVG.firstChild.setAttribute("height", "");
+                        tempSVG.firstChild.setAttribute("viewBox", "0 0 " + width + " " + height);
+    
+                        console.log(tempSVG.firstChild);
+    
+                        browser.tabs.sendMessage(tabs[0].id, {
+                            command: "addingPage",
+                            pageNum: i+1,
+                        })
+                        await SVGtoPDF(docKit, tempSVG.firstChild, 0, 0, {precision: 1});
+                        browser.tabs.sendMessage(tabs[0].id, {
+                            command: "addedPage",
+                            pageNum: i+1,
+                        })
+                    }
+                    if(i < data.urls.length-1) {
+                        docKit.addPage();
+                    }
                 }
                 
-            })();
+                let stream = docKit.pipe(blobStream());
+                stream.on('finish', async () => {
+                    let blob = stream.toBlob('application/pdf');
+                    downloadFile(data.info, data.type, URL.createObjectURL(blob));
+                    browser.tabs.sendMessage(tabs[0].id, {
+                        command: "dldSucc",
+                        data: ""
+                    });
+                    
+                });
+                docKit.end();
+                docKit = null;
+                docKit = new PDFDocument({compress: false, size:"A4"}); // new instance for new stream!
+            } catch (e) {
+                browser.tabs.sendMessage(tabs[0].id, {
+                    command: "backendError",
+                    error: e.message
+                }) 
+            }
+            
         }
+        return true;
     })    
 } catch (e) {
     browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
